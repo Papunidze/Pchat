8c5d936bf9279480271cbe8534e5582676527a9e
Papunidze <80461159+Papunidze@users.noreply.github.com>
2023-09-14 01:39:20 -0700

    feat:update-styles

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,11 +1,22 @@
 import chatBackground from "@/assets/images/chat-bg.png";
-import ChatCard from "@/modules/main/components/Card";
-import Search from "@/modules/main/components/Search";
+import ChatCard from "@/modules/main/components/card";
+import Search from "@/modules/main/components/search";
+import { useRef } from "react";
 
 const Main = () => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleTextareaChange = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      const newHeight = `${textareaRef.current.scrollHeight + 4}px `;
+      textareaRef.current.style.height = newHeight;
+    }
+  };
+
   return (
     <div className="flex w-full h-full">
-      <nav className="max-w-[420px] w-full  overflow-auto">
+      <nav className="max-w-[420px] w-full  overflow-auto border-r-[1px] border-gray-300 ">
         <div className="flex flex-col p-4 gap-3">
           <section className="search">
             <Search />
@@ -13,22 +24,29 @@ const Main = () => {
 
           <section className="user-messages flex-center flex-col gap-4">
             <ChatCard />
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
 
-            <ChatCard />
-            <ChatCard />
-            <ChatCard />
             <ChatCard />
           </section>
         </div>
       </nav>
-      <section className="flex-grow bg-gradient">
-        <section
+      <section className="flex-grow bg-gradient-green">
+        <div
           className="flex-grow w-full h-full"
           style={{ backgroundImage: `url(${chatBackground})` }}
-        ></section>
+        >
+          <div className="w-full h-[3.5rem] bg-white"></div>
+          <div className="w-full h-[calc(100%_-_3.5rem)] flex-col flex-between py-10">
+            <h1>1</h1>
+            <div className="flex-center w-full">
+              <textarea
+                ref={textareaRef}
+                className={`w-[250px] p-[15px] outline-none resize-none text-[16px] rounded-[5px] border-2 border-gray-500 focus:border-blue-500 overflow-auto  max-h-[450px]`}
+                rows={1}
+                onChange={handleTextareaChange}
+              ></textarea>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,47 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+/** @type {import('tailwindcss').Configuration} */
+const tailwindConfig = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontSize: {
-      xs: ["12px", "16px"],
-      sm: ["14px", "20px"],
-      base: ["16px", "19.5px"],
-      lg: ["18px", "21.94px"],
-      xl: ["20px", "24.38px"],
-      "2xl": ["24px", "29.26px"],
-      "3xl": ["28px", "50px"],
-      "4xl": ["48px", "58px"],
-      "8xl": ["96px", "106px"],
-    },
     extend: {
+      fontSize: {
+        "text-xs": ["12px", "16px"],
+        "text-sm": ["14px", "20px"],
+        "text-base": ["16px", "19.5px"],
+        "text-lg": ["18px", "21.94px"],
+        "text-xl": ["20px", "24.38px"],
+        "text-2xl": ["24px", "29.26px"],
+        "text-3xl": ["28px", "50px"],
+        "text-4xl": ["48px", "58px"],
+        "text-8xl": ["96px", "106px"],
+      },
       backgroundImage: {
-        gradient:
+        "gradient-green":
           "linear-gradient(to right bottom, #d5d88d, #6ba587, #88b884, #dbddbb)",
       },
       fontFamily: {
-        regular: [
-          "Montserrat,Mona Sans, Helvetica Neue, Helvetica,Graphik Web Regular, Arial",
+        "sans-regular": [
+          "Roboto",
+          "-apple-system",
+          "apple color emoji",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Oxygen-Sans",
+          "Ubuntu",
+          "Cantarell",
+          "Helvetica Neue",
           "sans-serif",
         ],
         montserrat: ["Montserrat", "sans-serif"],
       },
       colors: {
         primary: "#FF6452",
-        "dark-text": "#3d3d4e",
+        dark: "#3d3d4e",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
         "white-400": "rgba(255, 255, 255, 0.80)",
       },
       boxShadow: {
-        "main-shadow": "0px 4px 6px rgba(0, 0, 0, 0.1)",
+        1: "0px 4px 6px rgba(0, 0, 0, 0.1)",
       },
       screens: {
         sm: "640px",
@@ -45,3 +53,5 @@ export default {
   },
   plugins: [],
 };
+
+export default tailwindConfig;
