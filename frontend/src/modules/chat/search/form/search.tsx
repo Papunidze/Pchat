49bc886a6ab1858a9c00e560d74bd9d2e913a00b
Papunidze <80461159+Papunidze@users.noreply.github.com>
2023-10-25 06:30:24 -0700

    feat:chats

--- frontend/src/modules/chat/search/form/search.tsx
+++ frontend/src/modules/chat/search/form/search.tsx
@@ -4,8 +4,7 @@ import DropDown from "@/components/dropdown/drop-down";
 import { menuItems } from "../components/menu-items";
 import { generateMenuArray } from "../../components/menu-array";
 import { useMutation } from "react-query";
-import { useLocation, useNavigate } from "react-router-dom";
-import { search } from "../search-api";
+import { accessChat, search } from "../search-api";
 import CardSkeleton from "@/components/loaders/card-skeleton";
 import Avatar from "@/components/loaders/avatar-preloader";
 import { UserState } from "@/context/login-provider";
@@ -13,14 +12,11 @@ import { UserState } from "@/context/login-provider";
 type SearchUser = Pick<UserState, "_id" | "name" | "username" | "avatar">;
 
 const Search = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const flow = params.get("flow");
-  const [isFocus, setIsFocus] = useState(flow === "search" ? true : false);
+  const [isFocus, setIsFocus] = useState(false);
   const [searchResult, setSearchResult] = useState<SearchUser[] | null>([]);
   const [searchValue, setSearchValue] = useState("");
   const $searchQuery = useMutation(search);
+  const $accsesChatMutation = useMutation(accessChat);
   const handleSearch = (event: React.FormEvent<HTMLInputElement>) => {
     setSearchValue(event.currentTarget.value);
     if (event.currentTarget.value.length > 1) {
@@ -45,21 +41,14 @@ const Search = () => {
     setSearchResult(null);
     setSearchValue("");
   };
-  const handleFocuse = () => {
-    setIsFocus(true);
-    navigate("/?flow=search");
-  };
-  const handleBlur = () => {
-    setIsFocus(false);
-    navigate("/");
-  };
+
   return (
     <>
       <div className="flex w-full items-center justify-center gap-2">
         {isFocus ? (
           <button
             className="icon-button rotate-180 transition-all ease-in"
-            onClick={handleBlur}
+            onClick={() => setIsFocus(false)}
           >
             <Icon icon={"fa-solid fa-arrow-right"} />
           </button>
@@ -74,8 +63,7 @@ const Search = () => {
           <input
             placeholder="Search"
             className="border border-borderGray pl-11  flex-1 leading-10 rounded-3xl outline-none focus:border-blue-400 focus:border max-w-full transition-all ease-in bg-transparent  dark:text-white "
-            onFocus={handleFocuse}
-            onBlur={handleBlur}
+            onFocus={() => setIsFocus(true)}
             value={searchValue}
             onChange={handleSearch}
           />
@@ -91,39 +79,46 @@ const Search = () => {
           )}
         </div>
       </div>
-      {flow === "search" && !searchValue && (
+      {isFocus && !searchValue && (
         <div className="text-center mt-10">
           <h1 className="text-4xl text-primary font-bold">Search a Friend</h1>
         </div>
       )}
-      {isFocus &&
-        searchResult !== null &&
-        ($searchQuery.isLoading ? (
-          <div className="p-3">
-            <CardSkeleton />
-          </div>
-        ) : (
-          searchResult.map((element, index) => (
-            <div
-              className="flex flex-col items-center w-full justify-center mt-4 animate-fade"
-              key={index}
-            >
-              <a className="flex w-full items-center justify-start gap-3 p-3 cursor-pointer hover:bg-gray-200 rounded-lg">
-                <Avatar src={element.avatar} alt="avatar" style="" />
-                <div className="flex flex-col items-center justify-start w-full">
-                  <div className="self-start">
-                    <h1 className="overflow-hidden text-ellipsis whitespace-nowrap text-base font-semibold font-montserrat capitalize">
-                      {element.name}
-                    </h1>
-                  </div>
-                  <span className="overflow-hidden text-ellipsis whitespace-nowrap text-base leading-5 text-gray-400 pl-0 self-start font-montserrat">
-                    @{element.username}
-                  </span>
+      {isFocus && (
+        <div className="w-full absolute h-[calc(100%-4rem)] left-0 z-50 overflow-auto bg-white">
+          {searchResult !== null &&
+            ($searchQuery.isLoading ? (
+              <div className="p-3">
+                <CardSkeleton />
+              </div>
+            ) : (
+              searchResult.map((element, index) => (
+                <div
+                  className="flex flex-col items-center w-full justify-center mt-4 animate-fade"
+                  key={index}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    $accsesChatMutation.mutate({ _id: element._id });
+                  }}
+                >
+                  <a className="flex w-full items-center justify-start gap-3 p-3 cursor-pointer hover:bg-gray-200 rounded-lg">
+                    <Avatar src={element.avatar} alt="avatar" style="" />
+                    <div className="flex flex-col items-center justify-start w-full">
+                      <div className="self-start">
+                        <h1 className="overflow-hidden text-ellipsis whitespace-nowrap text-base font-semibold font-montserrat capitalize">
+                          {element.name}
+                        </h1>
+                      </div>
+                      <span className="overflow-hidden text-ellipsis whitespace-nowrap text-base leading-5 text-gray-400 pl-0 self-start font-montserrat">
+                        @{element.username}
+                      </span>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
-          ))
-        ))}
+              ))
+            ))}
+        </div>
+      )}
     </>
   );
 };
--- frontend/src/modules/chat/search/search-api.ts
+++ frontend/src/modules/chat/search/search-api.ts
@@ -1,9 +1,11 @@
 import { rest } from "@/lib/request";
-import { TSearch } from "./searching-codec";
+import { TSearch, TChat } from "./searching-codec";
 
-export type SearchInput = {
-  member: string;
-};
+export type SearchInput = { member: string };
+export type accessChatProps = { _id: string };
 
 export const search = ({ member }: SearchInput) =>
   rest.get(`/user/search?member=${member}`).decode(TSearch);
+
+export const accessChat = ({ _id }: accessChatProps) =>
+  rest.post("/chat", { _id }).decode(TChat);
